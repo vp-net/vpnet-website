569119cab48888f9c22e84337d0ebecdc8da4641
Nantha Bala P <95492208+Deadends@users.noreply.github.com>
2024-07-12 14:17:29 +0530

    padding-update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main className="bg-[#000000] flex min-h-screen flex-col items-center pt-10 ps-3 pe-3 px-24 sm:p-3">
+    <main className="bg-[#000000] flex min-h-screen flex-col items-center pt-20  ps-5 lg:px-24 sm:ps-2 pe-3 px-24 sm:p-3">
      
      {/* <App/> */}
      <GlobeDemo/>
--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -97,7 +97,7 @@ const Feature = ({
       </div>
       <div className="text-lg font-bold mb-2 relative z-10 px-10">
         <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral- group-hover/feature:bg-green-500 transition-all duration-200 origin-center" />
-        <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-green-400 dark:text-green-400">
+        <span className="group-hover/feature:translate-x-2 transition duration-200  inline-block text-green-400 dark:text-green-400">
           {title}
         </span>
       </div>
