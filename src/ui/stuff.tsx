1ab900e5c4a2e1825872ebb3dd8f1ec79b5ce579
Nantha Bala P <95492208+Deadends@users.noreply.github.com>
2024-07-15 20:03:58 +0530

    close to final

--- src/ui/stuff.tsx
+++ src/ui/stuff.tsx
@@ -14,12 +14,12 @@ export default function ParticlesDemo() {
 
    return (
       <div className="bg-black relative flex h-screen w-full flex-col items-center justify-center overflow-hidden">
-         <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl lg:text-9xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+         <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-7xl lg:text-9xl sm:text-2xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10 sm:p-4">
             VPNET
 
          </span>
          <p className="relative z-1  text-1xl md:text-1xl lg:text-1xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-              <br/>Vanish among the particles
+              Vanish among the stars
           </p>
          <Particles
             className="absolute inset-0"
