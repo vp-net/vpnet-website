1b37eb932af7663c378170df6c2c856aaf3d55f8
Nantha Bala P <95492208+Deadends@users.noreply.github.com>
2024-07-26 00:22:44 +0530

    push-03

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,21 +18,6 @@ const config = {
       },
     },
     extend: {
-  
-      animation: {
-          "meteor-effect": "meteor 5s linear infinite",
-        },
-        keyframes: {
-          meteor: {
-            "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
-            "70%": { opacity: "1" },
-            "100%": {
-              transform: "rotate(215deg) translateX(-500px)",
-              opacity: "0",
-            },
-          },
-        },
-      
       
       colors: {
         border: "hsl(var(--border))",
@@ -101,13 +86,6 @@ const config = {
 
   ],
  
-  theme: {
-    
-
-},
-
-
-
 } satisfies Config
 
 export default config
