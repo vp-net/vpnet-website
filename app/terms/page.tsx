44cf2f68bd69f9a96ae4f074ccaede7c2d3aac43
Nantha Bala P <95492208+Deadends@users.noreply.github.com>
2024-08-09 13:47:39 +0530

    legal-page-update

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -18,35 +18,35 @@ const page = () => {
 
 
         <p className='text-center p-5 pb-8 w-full h-full animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-3  font-extralight leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-.4xl md:text-2xl lg:text-2xl dark:from-white dark:to-white/60'>
-        Your use of the website and/or purchase from us are governed by following Terms and Conditions:</p>  
+        Your use of the website and/or purchase from us are governed by following Terms and Conditions :</p>  
 
         <p className='text-left p-2 pb-2 pl-20 pr-10 w-full h-full animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-3  font-extralight leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-1xl md:text-1xl lg:text-1xl dark:from-white dark:to-white/60'>
-        - The content of the pages of this website is subject to change without notice.</p>  
+        - The content of the pages of this website is subject to change without notice .</p>  
         
         <p className='text-left p-2 pb-2 pl-20 pr-10 w-full h-full animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-3  font-extralight leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-1xl md:text-1xl lg:text-1xl dark:from-white dark:to-white/60'>
-        - Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.</p>  
+        - Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law .</p>  
         
         <p className='text-left p-2 pb-2 pl-20  pr-10 w-full h-full animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-3  font-extralight leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-1xl md:text-1xl lg:text-1xl dark:from-white dark:to-white/60'>
-        - Your use of any information or materials on our website and/or product pages is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through our website and/or product pages meet your specific requirements.</p>
+        - Your use of any information or materials on our website and/or product pages is entirely at your own risk, for which we shall not be liable. It shall be your own responsibility to ensure that any products, services or information available through our website and/or product pages meet your specific requirements .</p>
         
         
         <p className='text-left p-2 pb-2 pl-20 pr-10 w-full h-full animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-3  font-extralight leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-1xl md:text-1xl lg:text-1xl dark:from-white dark:to-white/60'>
-        - Our website contains material which is owned by or licensed to us. This material includes, but are not limited to, the design, layout, look, appearance and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these terms and conditions.</p>  
+        - Our website contains material which is owned by or licensed to us. This material includes, but are not limited to, the design, layout, look, appearance and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these terms and conditions .</p>  
 
         <p className='text-left p-2 pb-2 pl-20 pr-10 w-full h-full animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-3  font-extralight leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-1xl md:text-1xl lg:text-1xl dark:from-white dark:to-white/60'>
-        - All trademarks reproduced in our website which are not the property of, or licensed to, the operator are acknowledged on the website.</p>  
-
-        
-        <p className='text-left p-2 pb-2 pl-20 pr-10 w-full h-full animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-3  font-extralight leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-1xl md:text-1xl lg:text-1xl dark:from-white dark:to-white/60'>
-        - Unauthorized use of information provided by us shall give rise to a claim for damages and/or be a criminal offense.</p>  
+        - All trademarks reproduced in our website which are not the property of, or licensed to, the operator are acknowledged on the website .</p>  
 
         
         <p className='text-left p-2 pb-2 pl-20 pr-10 w-full h-full animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-3  font-extralight leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-1xl md:text-1xl lg:text-1xl dark:from-white dark:to-white/60'>
-        - From time to time our website may also include links to other websites. These links are provided for your convenience to provide further information.</p>  
+        - Unauthorized use of information provided by us shall give rise to a claim for damages and/or be a criminal offense .</p>  
 
         
         <p className='text-left p-2 pb-2 pl-20 pr-10 w-full h-full animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-3  font-extralight leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-1xl md:text-1xl lg:text-1xl dark:from-white dark:to-white/60'>
-        - You may not create a link to our website from another website or document without ARSH VIMAL’s prior written consent.</p>  
+        - From time to time our website may also include links to other websites. These links are provided for your convenience to provide further information .</p>  
+
+        
+        <p className='text-left p-2 pb-2 pl-20 pr-10 w-full h-full animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-3  font-extralight leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-1xl md:text-1xl lg:text-1xl dark:from-white dark:to-white/60'>
+        - You may not create a link to our website from another website or document without ARSH VIMAL’s prior written consent .</p>  
 
         
         <p className='text-left p-2 pb-2 pl-20 pr-10 w-full h-full animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-3  font-extralight leading-none tracking-tighter text-transparent opacity-0 [--animation-delay:200ms] sm:text-1xl md:text-1xl lg:text-1xl dark:from-white dark:to-white/60'>
